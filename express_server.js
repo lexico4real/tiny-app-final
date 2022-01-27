47d380b8e744816e97a72cdf1e6b524e82de5705
Oluyinka Abubakar <oluyinkaabubakar@Oluyinkas-MacBook-Pro.local>
2022-01-28 00:46:36 +0100

    first commit

---
+++ express_server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const PORT = 8080;
+
+// a function which handles requests and sends response
+const requestHandler = function (request, response) {
+  console.log('In requestHandler'); // NEW LINE
+  response.end(
+    `Requested Path: ${request.url}\nRequest Method: ${request.method}`
+  );
+};
+
+const server = http.createServer(requestHandler);
+console.log('Server created'); // NEW LINE
+
+server.listen(PORT, () => {
+  console.log(`Server listening on: http://localhost:${PORT}`);
+});
+
+console.log('Last line (after .listen call)'); // NEW LINE
